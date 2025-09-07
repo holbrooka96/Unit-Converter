8c5585826a23e870c1e5565c499fe9214f9cafbf
Austin Holbrook <holbrooka96@gmail.com>
2025-09-07 11:44:34 -0400

    JS logic, styling updates, default rendering.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 /*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
+1 meter = 3.2808 feet
+1 liter = 0.2641720524 gallons
+1 kilogram = 2.2046226218 pounds
 */
-//20 meters = 65.616 feet | 20 feet = 6.096 meters
-//20 liters = 5.284 gallons | 20 gallons = 75.708 liters
-//20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
 
 const convertBtn = document.getElementById("convert-btn")
 const length = document.getElementById("length")
 const volume = document.getElementById("volume")
 const mass = document.getElementById("mass")
 const inputText = document.getElementById("input-box")
- 
+const meterConversionNum =  3.2808
+const literConversionNum =  0.2641720524
+const kgConversionNum =  2.2046226218
 
-convertBtn.addEventListener("click", function() {
-    numtoConvert = Number(inputText.value)
-    length.textContent =  `${numtoConvert} meters = ${(numtoConvert*3.281).toFixed(3)} feet 
-    | ${numtoConvert} feet = ${(numtoConvert/3.281).toFixed(3)} meters`
+
+convertBtn.addEventListener("click", convertNum)
+
+
+function convertNum() {
+numtoConvert = Number(inputText.value)
+
+    length.textContent =  `${numtoConvert} meters = ${(numtoConvert*meterConversionNum).toFixed(3)} feet 
+    | ${numtoConvert} feet = ${(numtoConvert/meterConversionNum).toFixed(3)} meters`
     
-    volume.textContent =  `${numtoConvert} liters = ${(numtoConvert*0.264).toFixed(3)} gallons 
-    | ${numtoConvert} gallons = ${(numtoConvert/0.264).toFixed(3)} liters`
+    volume.textContent =  `${numtoConvert} liters = ${(numtoConvert*literConversionNum).toFixed(3)} gallons 
+    | ${numtoConvert} gallons = ${(numtoConvert/literConversionNum).toFixed(3)} liters`
 
-    mass.textContent =  `${numtoConvert} kilos = ${(numtoConvert*2.205).toFixed(3)} pounds 
-    | ${numtoConvert} pounds = ${(numtoConvert/2.205).toFixed(3)} kilos`
+    mass.textContent =  `${numtoConvert} kilos = ${(numtoConvert*kgConversionNum).toFixed(3)} pounds 
+    | ${numtoConvert} pounds = ${(numtoConvert/kgConversionNum).toFixed(3)} kilos`
 
-})
+}
 
-
+convertNum()
